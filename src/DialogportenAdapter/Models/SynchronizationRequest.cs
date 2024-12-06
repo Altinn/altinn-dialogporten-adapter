fe5e90c09fd9099e47c1cc274369de8b375739b4
Magnus Sandgren <5285192+MagnusSandgren@users.noreply.github.com>
2024-12-06 15:05:05 +0100

    WIP

--- src/DialogportenAdapter/Models/SynchronizationRequest.cs
+++ src/DialogportenAdapter/Models/SynchronizationRequest.cs
@@ -2,8 +2,5 @@ namespace DialogportenAdapter.Models;
 
 public class SynchronizationRequest
 {
-    public required string ApplicationId { get; init; }
-    public required string InstanceId { get; init; }
+    public string InstanceId { get; set; } = null!;
 }
-
-public record Lala(Guid ApplicationId, Guid InstanceId);
