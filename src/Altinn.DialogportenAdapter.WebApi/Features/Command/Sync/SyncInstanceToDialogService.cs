9a448250ad3ad056f84dd7dce497215b3c82b50f
Magnus Sandgren <5285192+MagnusSandgren@users.noreply.github.com>
2025-03-14 12:33:23 +0100

    WIP

--- src/Altinn.DialogportenAdapter.WebApi/Features/Command/Sync/SyncInstanceToDialogService.cs
+++ src/Altinn.DialogportenAdapter.WebApi/Features/Command/Sync/SyncInstanceToDialogService.cs
@@ -7,9 +7,6 @@ using Altinn.Platform.Storage.Interface.Models;
 
 namespace Altinn.DialogportenAdapter.WebApi.Features.Command.Sync;
 
-// instanceId: partyId/instanceGuid
-// appId: appOwner/appId:int
-
 public record SyncInstanceToDialogDto(
     string AppId,
     int PartyId,
