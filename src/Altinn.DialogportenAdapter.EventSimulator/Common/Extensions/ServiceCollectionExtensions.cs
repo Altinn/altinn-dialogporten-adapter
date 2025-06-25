96894c135b9f39ea635c3b7a334b178eca2a2dcc
Magnus Sandgren <5285192+MagnusSandgren@users.noreply.github.com>
2025-06-25 09:47:44 +0200

    WIP

--- src/Altinn.DialogportenAdapter.EventSimulator/Common/Extensions/ServiceCollectionExtensions.cs
+++ src/Altinn.DialogportenAdapter.EventSimulator/Common/Extensions/ServiceCollectionExtensions.cs
@@ -5,60 +5,38 @@ namespace Altinn.DialogportenAdapter.EventSimulator.Common.Extensions;
 
 internal static class ServiceCollectionExtensions
 {
-    public static IServiceCollection ReplaceTransient<TService, TImplementation>(
-        this IServiceCollection services,
-        bool predicate = true)
+    public static IServiceCollection ReplaceTransient<TService, TImplementation>(this IServiceCollection services)
         where TService : class
         where TImplementation : class, TService =>
-        services.Replace<TService, TImplementation>(ServiceLifetime.Transient, predicate);
+        services.Replace<TService, TImplementation>(ServiceLifetime.Transient);
 
-    public static IServiceCollection ReplaceScoped<TService, TImplementation>(
-        this IServiceCollection services,
-        bool predicate = true)
+    public static IServiceCollection ReplaceScoped<TService, TImplementation>(this IServiceCollection services)
         where TService : class
         where TImplementation : class, TService =>
-        services.Replace<TService, TImplementation>(ServiceLifetime.Scoped, predicate);
+        services.Replace<TService, TImplementation>(ServiceLifetime.Scoped);
 
-    public static IServiceCollection ReplaceSingleton<TService, TImplementation>(
-        this IServiceCollection services,
-        bool predicate = true)
+    public static IServiceCollection ReplaceSingleton<TService, TImplementation>(this IServiceCollection services)
         where TService : class
         where TImplementation : class, TService =>
-        services.Replace<TService, TImplementation>(ServiceLifetime.Singleton, predicate);
+        services.Replace<TService, TImplementation>(ServiceLifetime.Singleton);
 
     private static IServiceCollection Replace<TService, TImplementation>(
         this IServiceCollection services,
-        ServiceLifetime lifetime = ServiceLifetime.Scoped,
-        bool predicate = true)
+        ServiceLifetime lifetime)
         where TService : class
         where TImplementation : class, TService
     {
-        if (!predicate)
-        {
-            return services;
-        }
-
         var serviceType = typeof(TService);
         var implementationType = typeof(TImplementation);
-
         // Remove all matching service registrations
-        foreach (var descriptor in services
-            .Where(x => x.ServiceType == serviceType)
-            .ToList())
+        for (var i = services.Count - 1; i >= 0; i--)
         {
-            services.Remove(descriptor);
+            if (services[i].ServiceType == serviceType) services.RemoveAt(i);
         }
 
         services.Add(ServiceDescriptor.Describe(serviceType, implementationType, lifetime));
-
         return services;
     }
-
-    public static IServiceCollection AddHostedService<THostedService>(this IServiceCollection services, bool predicate)
-        where THostedService : class, IHostedService =>
-        predicate
-            ? services.AddHostedService<THostedService>()
-            : services;
 
     public static IHostApplicationBuilder ReplaceLocalDevelopmentResources(this IHostApplicationBuilder builder)
     {
@@ -69,10 +47,16 @@ internal static class ServiceCollectionExtensions
         }
 
         builder.Services
-            .RemoveAllImplementationTypes(typeof(AzureTableStartupLoader))
-            .ReplaceTransient<IMigrationPartitionRepository, MockMigrationPartitionRepository>(opt.DisableAzureStorage);
+            .DoIf(opt.DisableAzureStorage, x => x.ReplaceTransient<IMigrationPartitionRepository, MockMigrationPartitionRepository>())
+            .DoIf(opt.DisableAzureStorage, x => x.RemoveAllImplementationTypes(typeof(AzureTableStartupLoader)));
 
         return builder;
+    }
+
+    private static IServiceCollection DoIf(this IServiceCollection services, bool predicate, Action<IServiceCollection> action)
+    {
+        if (predicate) action(services);
+        return services;
     }
 
     private static IServiceCollection RemoveAllImplementationTypes(this IServiceCollection collection, Type implementationType)
