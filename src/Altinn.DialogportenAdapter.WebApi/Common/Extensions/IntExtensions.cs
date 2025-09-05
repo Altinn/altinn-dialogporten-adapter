b4e3a733248c6a1a7490be801381da7de0066f5e
Bjørn Dybvik Langfors <bdl@digdir.no>
2025-09-05 15:12:40 +0200

    Improve percentage approximation

--- src/Altinn.DialogportenAdapter.WebApi/Common/Extensions/IntExtensions.cs
+++ src/Altinn.DialogportenAdapter.WebApi/Common/Extensions/IntExtensions.cs
@@ -2,5 +2,7 @@ namespace Altinn.DialogportenAdapter.WebApi.Common.Extensions;
 
 public static class IntExtensions
 {
-    public static int PercentOf(this int value, int percent) => value * percent / 100;
+    // Gives an int approximation, ensure that we always get at least 1 and round up.
+    public static int PercentOf(this int value, int percent)
+        => Math.Max((int)Math.Ceiling((decimal)value * percent / 100), 1);
 }
