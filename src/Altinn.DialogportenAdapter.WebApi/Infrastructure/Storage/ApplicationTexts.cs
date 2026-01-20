fc347a717bc9d389240b2b0fb7c1f3d168fdca7a
Mats Jørgensen <mats.jorgensen@digdir.no>
2026-01-20 10:46:48 +0100

    Remove comment

--- src/Altinn.DialogportenAdapter.WebApi/Infrastructure/Storage/ApplicationTexts.cs
+++ src/Altinn.DialogportenAdapter.WebApi/Infrastructure/Storage/ApplicationTexts.cs
@@ -2,7 +2,6 @@ namespace Altinn.DialogportenAdapter.WebApi.Infrastructure.Storage;
 
 public class ApplicationTexts
 {
-    //public List<ApplicationTextsTranslation> Translations { get; set; } = new();
     public Dictionary<string, ApplicationTextsTranslation> Translations { get; set; } = new();
 }
 
