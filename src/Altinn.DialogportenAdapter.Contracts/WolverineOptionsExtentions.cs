2950b9a3a793f423498bd62dc81d53c8f3a2e312
Mats Jørgensen <mats.jorgensen@digdir.no>
2026-02-26 14:09:36 +0100

    Harden string null/whitespace check

--- src/Altinn.DialogportenAdapter.Contracts/WolverineOptionsExtentions.cs
+++ src/Altinn.DialogportenAdapter.Contracts/WolverineOptionsExtentions.cs
@@ -20,7 +20,7 @@ public static class WolverineOptionsExtentions
         opts.EnableAutomaticFailureAcks = false;
         opts.EnableRemoteInvocation = false;
         opts.MultipleHandlerBehavior = MultipleHandlerBehavior.Separated;
-        if (managementConnectionString != null)
+        if (string.IsNullOrWhiteSpace(managementConnectionString))
         {
             var transport = opts.Transports.GetOrCreate<AzureServiceBusTransport>();
             transport.ManagementConnectionString = managementConnectionString;
