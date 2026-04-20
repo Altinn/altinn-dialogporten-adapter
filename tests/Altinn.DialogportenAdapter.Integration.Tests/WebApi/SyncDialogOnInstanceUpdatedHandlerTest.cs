ed492f748263552dda038e7dd1eed1d2f4cc2667
Mats Jørgensen <mats.jorgensen@digdir.no>
2026-04-20 15:17:04 +0200

    Reduce wait time for indefinite retry test

--- tests/Altinn.DialogportenAdapter.Integration.Tests/WebApi/SyncDialogOnInstanceUpdatedHandlerTest.cs
+++ tests/Altinn.DialogportenAdapter.Integration.Tests/WebApi/SyncDialogOnInstanceUpdatedHandlerTest.cs
@@ -221,8 +221,8 @@ public class SyncDialogOnInstanceUpdatedHandlerTest(DialogportenAdapterApplicati
         ));
 
         // Assert
-        var dlqMessage = WaitForDlqMessage(TimeSpan.FromMilliseconds(7000));
-        var dialogLog = WaitForDialogPostedLogEntry(TimeSpan.FromMilliseconds(7000));
+        var dlqMessage = WaitForDlqMessage(TimeSpan.FromMilliseconds(4000));
+        var dialogLog = WaitForDialogPostedLogEntry(TimeSpan.FromMilliseconds(4000));
         await Task.WhenAll(dlqMessage, dialogLog);
 
         dlqMessage?.Result.Should().BeNull();
