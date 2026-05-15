2433115cb0d2e61e03bf0e941823ad0764453791
Mats Jørgensen <mats.jorgensen@digdir.no>
2026-05-15 13:39:10 +0200

    Ignore 404's from /purge dialog

--- src/Altinn.DialogportenAdapter.WebApi/Common/PartyNotFoundException.cs
+++ src/Altinn.DialogportenAdapter.WebApi/Common/PartyNotFoundException.cs
@@ -1,3 +1,3 @@
-namespace Altinn.DialogportenAdapter.WebApi.Common.Exceptions;
+namespace Altinn.DialogportenAdapter.WebApi.Common;
 
 public class PartyNotFoundException(string? partyId) : InvalidOperationException($"Party with id {partyId} not found");
